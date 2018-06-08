315715bb6d914b6df4ac3e99f1edad30ecbab029
Ben Burke <bernard.francis.burke@gmail.com>
2018-06-08 10:02:24 +1000

    Initial add

---
+++ unix/env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ "$ITSROOT" = "" ]; then
+	export ITSROOT="/its"
+fi
+if [ "$DEBUG" = "true" ]; then
+	set -xv
+fi
+
+export ITSS=$ITSROOT/scripts
+export ITSL=$ITSROOT/logs
+export ITSD=$ITSROOT/data
+export ITSE=$ITSROOT/env
+
+
+
+PATH="$PATH:$ITSS"
