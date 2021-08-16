e65bfe0857ebcbe837f82fd572aeca79f6394634
Ben Burke <bernard.francis.burke@gmail.com>
2021-08-17 06:21:06 +1000

    added overview

--- itsdocs/cnv.py
+++ itsdocs/cnv.py
@@ -1,6 +1,6 @@
 from markdownify import markdownify
 
-file = open("ITS overview.html", "r").read()
+file = open("itsdocs/ITS overview.html", "r").read()
 
 html = markdownify(file, heading_style="ATX")
 
